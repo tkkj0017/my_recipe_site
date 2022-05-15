9b4505462ebf2ae3b92a9e260ec6025dc36c4023
tkkj0017 <tttvvvaken@gmail.com>
2022-05-15 13:09:43 +0900

    Add TopPage View

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from recipe.views import RecipeListView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path("", RecipeListView.as_view(), name="index")
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Recipe
 
-# Create your views here.
+
+class RecipeListView(ListView):
+    model = Recipe
