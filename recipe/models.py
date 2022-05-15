c8541b966c2a66bcbb732de118128a1ef868c0e2
tkkj0017 <tttvvvaken@gmail.com>
2022-05-15 12:13:44 +0900

    first commit

---
+++ recipe/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Recipe(models.Model):
+    
+    class Meta:
+        verbose_name = "レシピ"
+        verbose_name_plural = "レシピ"
+        
+    def __str__(self):
+        return self.title
+    
+    title = models.CharField(verbose_name="タイトル", max_length=200)
+    content = models.TextField(verbose_name="内容")
+    
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
