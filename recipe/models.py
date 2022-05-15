bd51a7c34e3fbee218f7370dfaa115d944cfa93a
tkkj0017 <tttvvvaken@gmail.com>
2022-05-15 15:08:15 +0900

    トップページ修正

--- recipe/models.py
+++ recipe/models.py
@@ -13,5 +13,7 @@ class Recipe(models.Model):
     title = models.CharField(verbose_name="タイトル", max_length=200)
     content = models.TextField(verbose_name="内容")
     
+    description = models.TextField(blank=True, default="")
+    
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
