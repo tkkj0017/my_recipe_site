9af74f38e1d47d9aa6f38fe5d9f582553d1841dc
tkkj0017 <tttvvvaken@gmail.com>
2022-05-20 20:49:20 +0900

    ログイン機能追加

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,10 +24,13 @@ from lib.views import IndexTemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('', IndexTemplateView.as_view(), name="index"),
-    
     path('recipe/', include("recipe.urls")),
     path('comment/', include("comment.urls", namespace="comment")),
-    path('adminpage/', include("adminpage.urls", namespace="adminpage"))
+    path('adminpage/', include("adminpage.urls", namespace="adminpage")),
+    
+    path('login/', LoginView.as_view(template_name="login.html"), name="login"),
+    path('logout', LogoutView.as_view(template_name="logout.html"), name="logout"),
+    
+    path('', IndexTemplateView.as_view(), name="index"),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
