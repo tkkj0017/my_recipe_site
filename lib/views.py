e5327a947c133f22a34f9575ee919798476706be
tkkj0017 <tttvvvaken@gmail.com>
2022-05-20 21:17:01 +0900

    テストコード追加

--- lib/views.py
+++ lib/views.py
@@ -8,8 +8,12 @@ class IndexTemplateView(TemplateView):
     
     def get_context_data(self, **kwargs):
         context: dict = super().get_context_data(**kwargs)
-        
-        context['recipe_top'] = Recipe.objects.order_by('?')[0]
+
+        try:
+            context['recipe_top'] = Recipe.objects.order_by('?')[0]
+        except IndexError:
+            context['recipe_top'] = None
+            
         context['recipe_list'] = Recipe.objects.order_by("-created")
         
         return context
